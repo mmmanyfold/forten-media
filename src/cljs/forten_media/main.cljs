244f38ec138abd1ea293d4bb1240737992eb9e0f
carolina5267 <carolina.cruz@students.mapleton.us>
2017-10-24 13:23:46 -0600

    adds about-us view

--- src/cljs/forten_media/main.cljs
+++ src/cljs/forten_media/main.cljs
@@ -1,14 +1,17 @@
 (ns forten-media.views
   (:require [re-frame.core :as re-frame]
             [re-com.core :as re-com]
-            [forten-media.subs :as subs]))
+            [forten-media.subs :as subs]
+            [forten-media.views.about-us :refer [about-us-view]]
+            ))
+
 
 ;; home
 
 (defn home-title []
   (let [name (re-frame/subscribe [::subs/name])]
     [re-com/title
-     :label (str "Hello from " @name ". This is the Home Page.")
+     :label (str "HELLO from " @name ". This is the Home Page.")
      :level :level1]))
 
 (defn link-to-about-page []
@@ -22,30 +25,12 @@
    :children [[home-title] [link-to-about-page]]])
 
 
-;; about
-
-(defn about-title []
-  [re-com/title
-   :label "This is the About Page."
-   :level :level1])
-
-(defn link-to-home-page []
-  [re-com/hyperlink-href
-   :label "Something Else !!!!"
-   :href "#/"])
-
-(defn about-panel []
-  [re-com/v-box
-   :gap "1em"
-   :children [[about-title] [link-to-home-page]]])
-
-
 ;; main
 
 (defn- panels [panel-name]
   (case panel-name
     :home-panel [home-panel]
-    :about-panel [about-panel]
+    :about-panel [about-us-view]]
     [:div]))
 
 (defn show-panel [panel-name]
---
+++ src/cljs/forten_media/views/about_us.cljs
@@ -0,0 +1,8 @@
+(ns forten-media.views.about-us
+  (:require [re-frame.core :as re-frame]
+            [re-com.core :as re-com]
+            [forten-media.subs :as subs]
+            ))
+
+(defn about-us-view []
+  [:div "this is the about us view"])
