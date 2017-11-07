80e483ef1a1de122e557ea8a61223d114342974d
carolina5267 <carolina.cruz@students.mapleton.us>
2017-11-07 14:21:55 -0700

    begins building & styling about page

--- src/cljs/forten_media/views/about_us.cljs
+++ src/cljs/forten_media/views/about_us.cljs
@@ -5,4 +5,12 @@
 
 
 (defn about-us-view []
-  [:div "this is the about us view"])
+  [:div.wrapper
+   [:div.left "this is the about us view"]
+   [:div.right
+    [:div.display
+     [:img {:src "../img/recipe.png"}]
+     [:h2 "burger bowl recipe, " [:span "2017"]]
+     [:h3 "form star living"]
+     [:h3 "video"]
+     [:h3 "1080x1080"]]]])
