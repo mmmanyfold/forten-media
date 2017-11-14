7201a1e94ab203f8ba21640b90d789c60ecf801a
IsmaelT140 <32679870+IsmaelT140@users.noreply.github.com>
2017-11-14 12:47:54 -0700

    Layout left panel of about page (#9)

--- src/cljs/forten_media/views/about_us.cljs
+++ src/cljs/forten_media/views/about_us.cljs
@@ -6,7 +6,14 @@
 
 (defn about-us-view []
   [:div.wrapper
-   [:div.left "this is the about us view"]
+   [:div.left
+    [:div.w-80.text
+     [:h1 "OUR " [:span "WORK"]]
+     [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus lacus quis lobortis feugiat. Duis in pharetra sapien. Sed quis velit quis nibh rutrum auctor. Etiam turpis arcu, varius eget ante et, tincidunt luctus risus. In ac dapibus tortor, fringilla posuere augue. Nullam nunc metus, gravida a libero ut, commodo suscipit massa. Phasellus ultricies placerat lectus, at aliquet leo convallis eget. Fusce non mattis elit. Cras id scelerisque lectus. Proin orci libero, scelerisque nec commodo at, aliquet nec ex. Proin feugiat et tortor hendrerit efficitur. Integer in lectus lacus. Nulla ac cursus massa. Sed massa augue, ullamcorper pellentesque sapien sed, viverra egestas metus. Maecenas aliquet magna tellus, a semper dolor malesuada a."]
+     [:div.footer
+      [:div "< table of contents"]
+      [:div {:style {:text-align "center"}} "14"]
+      [:div ""]]]]
    [:div.right
     [:div.display.w-40
      [:img {:src "../img/recipe.png"}]
