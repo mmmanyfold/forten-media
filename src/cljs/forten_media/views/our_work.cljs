18713c22d9303ac3c086f479d0bb70dcb247fd62
eemshi <oreneemshi@gmail.com>
2018-02-06 11:37:46 -0700

    add .w-100-small class

--- src/cljs/forten_media/views/our_work.cljs
+++ src/cljs/forten_media/views/our_work.cljs
@@ -7,17 +7,17 @@
 (defn our-work-view []
   [:div.wrapper
    [:div.left
-    [:div.w-80.text
+    [:div.w-80.text.w-100-small
      [:h1 "OUR " [:span "WORK"]]
      [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus lacus quis lobortis feugiat. Duis in pharetra sapien. Sed quis velit quis nibh rutrum auctor. Etiam turpis arcu, varius eget ante et, tincidunt luctus risus. In ac dapibus tortor, fringilla posuere augue. Nullam nunc metus, gravida a libero ut, commodo suscipit massa. Phasellus ultricies placerat lectus, at aliquet leo convallis eget. Fusce non mattis elit. Cras id scelerisque lectus. Proin orci libero, scelerisque nec commodo at, aliquet nec ex. Proin feugiat et tortor hendrerit efficitur. Integer in lectus lacus. Nulla ac cursus massa. Sed massa augue, ullamcorper pellentesque sapien sed, viverra egestas metus. Maecenas aliquet magna tellus, a semper dolor malesuada a."]
-     [:div.footer-wrap.w-80
+     [:div.footer-wrap.w-80.w-100-small
       [:div.footer
        [:div [:a {:href "/#/"}
               "< table of contents"]]
        [:div {:style {:text-align "center"}} "14"]
        [:div ""]]]]]
    [:div.right
-    [:div.display.w-40
+    [:div.display.w-40.w-100-small
      [:img {:src "../img/recipe.png"}]
      [:h2 "burger bowl recipe, " [:span "2017"]]
      [:h3 "form star living"]
